ceda189634402ff38d005b845d2a05ed77c17c51
BishantRajbanshi <rajbancbishant@gmail.com>
2024-10-23 13:00:43 +0545

    updated task add gui

--- to_do_list.py
+++ to_do_list.py
@@ -17,4 +17,20 @@ class list_of_task:
     else:
       print("Task not found")
 
+def print_task(list_of_tasks):
+  tasks = list_of_task.show_list()
+  if tasks:
+    print("Tasks list: ")
+    for index,task in enumerate(tasks, start=1):
+      print(f" {index}. {task}")
 
+def add_task_gui():
+  task_text = task_entry.get().strip()
+  if task_text:
+    task_list.add_task(task_text)
+    update_task_list()
+
+def delete_task_gui():
+  index = task_listbox.curselection()
+  if index:
+    task_list.delete_task(index[0])
